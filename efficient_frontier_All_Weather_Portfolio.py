5bbd37f25a0d2515df17b24d0af842effcf37075
codebummer <holysmokes@hanmail.net>
2023-01-13 15:48:29 +0900

    Create efficient_frontier_All_Weather_Portfolio.py

---
+++ efficient_frontier_All_Weather_Portfolio.py
@@ -0,0 +1,66 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import pandas_datareader.data as web
+import yfinance as yf
+from datetime import datetime
+
+start = datetime(1980, 1, 1)
+end = datetime.today()
+years = end.year - start.year
+
+yf.pdr_override()
+def fetch_prices(stocks):
+    df = pd.DataFrame([])
+    for ticker, stock in stocks.items():
+         df[stock] = (web.get_data_yahoo(ticker, start, end)['Close'])
+    return df
+
+stocks = {
+  'SPY' : 'US Stocks',
+  'EFA' : 'Non-US Dveloped Market Stocks',
+  'EEM' : 'Emerging Market Stocks',
+  'DBC' : 'Commodities',
+  'GLD' : 'Gold',
+  'EDV' : 'Extended Duration Teasuries',
+  'LTPZ' : 'Tresuary Inflation-Protected Securities',
+  'LQD' : 'US Corporate Bonds',
+  'EMLC' : 'Emerging Market Bonds'
+}
+
+df = fetch_prices(stocks)
+
+# BIZDAYS_A_YEAR = (end-start).days / years #This is wrong
+BIZDAYS_A_YEAR = 252
+daily_ret = df.pct_change().add(1).cumprod()
+annual_ret = np.power(daily_ret[-1], 1/years) - 1
+daily_cov = df.pct_change().cov()
+annual_cov = daily_cov * BIZDAYS_A_YEAR
+
+port_ret, port_risk, port_weights = [], [], []
+
+for _ in range(20_000):
+    weights = np.random.random(len(stocks))
+    weights /= np.sum(weights)
+    
+    returns = np.dot(weights, annual_ret)
+    risk = np.sqrt(np.dot(weights.T, np.dot(annual_cov, weights)))
+    # risk = np.sqrt(np.multiply(weights.T, np.dot(annual_cov, weights))) # same as above
+    # risk = np.sqrt(weights.T * np.dot(annual_cov, weights))) # same as above
+    
+    port_ret.append(returns)
+    port_risk.append(risk)
+    port_weights.append(weights)
+
+portfolio = {'Returns' : port_ret, 'Risk' : port_risk}
+for i, s in enumerate(stocks.values()):
+    portfolio[s] = [weight[i] for weight in port_weights]
+
+df = pd.DataFrame(portfolio)
+df = df[['Returns', 'Risk'] + [s for s in stocks.values()]]
+
+df.plot.scatter(x='Risk', y='Returns', figsize=(8,6), grid=True)
+plt.title('Efficient Frontier')
+plt.xlabel('Risk')
+plt.ylabel('Expected Returns')
+plt.show()
