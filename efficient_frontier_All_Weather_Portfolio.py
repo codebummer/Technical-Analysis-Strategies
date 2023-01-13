49d201ac79be526e8c7e244a7f0735fedad2bb54
codebummer <holysmokes@hanmail.net>
2023-01-13 16:13:18 +0900

    Update efficient_frontier_All_Weather_Portfolio.py

--- efficient_frontier_All_Weather_Portfolio.py
+++ efficient_frontier_All_Weather_Portfolio.py
@@ -4,10 +4,11 @@ import matplotlib.pyplot as plt
 import pandas_datareader.data as web
 import yfinance as yf
 from datetime import datetime
+import sqlite3
 
-start = datetime(1980, 1, 1)
+start = datetime(2010, 7, 23)
 end = datetime.today()
-years = end.year - start.year
+years = end.year - start.year + 0.5
 
 yf.pdr_override()
 def fetch_prices(stocks):
@@ -30,10 +31,16 @@ stocks = {
 
 df = fetch_prices(stocks)
 
+with sqlite3.connect('allweather_portfolio.db') as db:
+  df.to_sql('All_Weather_Portfolio', db, if_exists='replace')
+
+# with sqlite3.connect('allweather_portfolio.db') as db:
+#   df = pd.read_sql('SELECT * from [All_Weather_Portfolio]', db)  
+
 # BIZDAYS_A_YEAR = (end-start).days / years #This is wrong
 BIZDAYS_A_YEAR = 252
 daily_ret = df.pct_change().add(1).cumprod()
-annual_ret = np.power(daily_ret[-1], 1/years) - 1
+annual_ret = np.power(daily_ret[-1:], 1/years) - 1
 daily_cov = df.pct_change().cov()
 annual_cov = daily_cov * BIZDAYS_A_YEAR
 
@@ -43,7 +50,7 @@ for _ in range(20_000):
     weights = np.random.random(len(stocks))
     weights /= np.sum(weights)
     
-    returns = np.dot(weights, annual_ret)
+    returns = np.dot(weights, annual_ret.iloc[0])
     risk = np.sqrt(np.dot(weights.T, np.dot(annual_cov, weights)))
     # risk = np.sqrt(np.multiply(weights.T, np.dot(annual_cov, weights))) # same as above
     # risk = np.sqrt(weights.T * np.dot(annual_cov, weights))) # same as above
