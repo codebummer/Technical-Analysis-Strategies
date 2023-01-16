9a12f9ce1253ddaab8a46de6e1c3707218d8e400
codebummer <holysmokes@hanmail.net>
2023-01-16 19:12:49 +0900

    Update returns_All_Weather_Portfolio1.3.py

--- returns_All_Weather_Portfolio1.3.py
+++ returns_All_Weather_Portfolio1.3.py
@@ -62,7 +62,112 @@ weights = {
     'EMLC' : 7
 }   
 # create a series instead of a dataframe, 
-# because when multiplying, indices are often different, 
+# because when multiplying, index names (or column names) are often different, 
+# which causes incorrect results 
+weights = pd.Series(weights) 
+
+invested = 10_000
+
+# use the following statement, when weights is defined as a dataframe. 
+# Because the indices are different, 
+# multiplying, indices should be removed
+# weights.iloc[0,:] * prices.head() 
+
+
+# The following immediate block is just for initial plotting purposes.
+weighted_prices = weights*prices
+daily_amounts = weighted_prices.sum(axis='columns')
+sns.set()
+fig, ax = plt.subplots(1,2)
+ax[0].plot(daily_amounts)
+ax[1].plot(weighted_prices)
+plt.legend(weighted_prices.columns)
+plt.show()
+
+holdings = invested * weights / prices.iloc[0,:]
+holdings = holdings.astype('int')
+holdings = holdings.to_frame(name=pd.Timestamp(start))
+holdings = holdings.T
+
+values = holdings * prices.iloc[0,:]
+
+yearly_prices = pd.DataFrame(prices.iloc[0,:], columns=[prices.index[0]])
+for year in years:
+    yearly_prices = pd.concat([yearly_prices, prices.loc[prices.index.year==year].iloc[-1,:]], axis='columns')
+yearly_prices = yearly_prices.T
+
+daily_ret = prices.pct_change()
+yearly_cumprod = pd.DataFrame()
+for year in years:
+    # yearly_cumprod = pd.concat([yearly_cumprod, daily_ret.loc[daily_ret.index.year==year].add(1).cumprod()])
+    yearly_cumprod = pd.concat([yearly_cumprod, daily_ret.loc[daily_ret.index.year==year].add(1).cumprod().iloc[-1,:]], axis='columns')
+yearly_cumprod = yearly_cumprod.T
+
+import pandas as pd
+import pandas_datareader.data as web
+import yfinance as yf
+import numpy as np
+from datetime import datetime
+import sqlite3
+import matplotlib.pyplot as plt
+import seaborn as sns
+import os
+os.chdir(r'D:\myprojects')
+
+start = datetime(2010, 7, 23)
+end = datetime.today()
+years = tuple(range(start.year, end.year+1))
+
+yf.pdr_override()
+def fetch_prices(stocks):
+    df = pd.DataFrame([])
+    for ticker, stock in stocks.items():
+        # df[stock] = (web.get_data_yahoo(ticker, start, end)['Close'])
+        df[ticker] = (web.get_data_yahoo(ticker, start, end)['Close'])
+    return df
+
+stocks = {
+    'SPY' : 'US Stocks',
+    'EFA' : 'Non-US Dveloped Market Stocks',
+    'EEM' : 'Emerging Market Stocks',
+    'DBC' : 'Commodities',
+    'GLD' : 'Gold',
+    'EDV' : 'Extended Duration Teasuries',
+    'LTPZ' : 'Tresuary Inflation-Protected Securities',
+    'LQD' : 'US Corporate Bonds',
+    'EMLC' : 'Emerging Market Bonds',
+    # 'KRW=X' : 'USD/KRW'
+}
+
+prices = fetch_prices(stocks)
+
+krw = fetch_prices({'KRW=X':'USD/KRW'})
+krw['change'] = krw.pct_change()
+krw_cumprod = pd.DataFrame()
+for year in years:
+    krw_cumprod = pd.concat([krw_cumprod, krw.loc[krw.index.year==year,'change'].add(1).cumprod()])
+krw = pd.concat([krw,krw_cumprod], axis='columns')
+krw.columns = list(krw.columns)[:-1]+['cumprod']
+
+with sqlite3.connect('allweather_portfolio.db') as db:
+    prices.to_sql('Stock_Prices', db, if_exists='replace')
+
+# for date in prices.index:
+#     prices.loc[date, 'DayofYear'] = pd.Period(date, freq='D').day_of_year
+
+weights = {
+    'SPY' : 12,
+    'EFA' : 12,
+    'EEM' : 12,
+    'DBC' : 7,
+    'GLD' : 7,
+    'EDV' : 18,
+    'LTPZ' : 18,
+    'LQD' : 7,
+    'EMLC' : 7
+}   
+# create a series instead of a dataframe, 
+# because when multiplying, index names (or column names) are often different, 
 # which causes incorrect results 
 weights = pd.Series(weights) 
 
@@ -112,21 +217,54 @@ for yearend in yearly_prices.index:
     
     add = values.loc[yearend]/yearly_prices.loc[yearend]
     holdings = pd.concat([holdings, add.round(decimals=1).astype('int').to_frame().T])
-
+    
+    # The rebalancing algorithm is as follow:
+    # 1. find off values = calculate prices lower or higher than allocation plans
+    #   off values = yearend prices - yearend prices * allocation ratio
     off_values = values.loc[yearend] - weights/100*values.loc[yearend].sum()
+    
+    # 2. find off quantities = calculate asset quantities lower or higher than allocation plans, based on off values
+    #   off quantities = off values / year end prices
+    #   round the above off quantities
+    #   convert them to integer
     off_qty = (off_values/yearly_prices.loc[yearend]).round(decimals=1).astype('int')   
     
-    gains = off_qty.clip(lower=0) * yearly_prices.loc[yearend]
-    losses = off_qty.clip(upper=0) * yearly_prices.loc[yearend]
-    # changes elements, which results NaN in later operations
-    # use clip() so as to keep elements intact, which results no NaN in later operations
-    # so, the following statements are not recommended
-    # gains = off_qty.loc[off_qty>0] * yearly_prices.loc[yearend,off_qty>0] 
-    # losses = off_qty.loc[off_qty<0] * yearly_prices.loc[yearend,off_qty<0]
     
+    # 3. find gains = calculate money amounts of assets higher than allocation plans, based on off quantities
+    #   make all minus off quantities 0 = use pandas.clip(lower=0)
+    #   dataframe.clip(lower=0) will keep all elements and prevent NaNs for matrix operations
+    #   dataframe.loc[year,off_qty<0] will remove all elements that do not match the condition, 
+    #   which will result NaNs and NaNs will cause errors for matrix operations.
+    #   The following is an example of the bad practice described above
+    #   gains = off_qty.loc[off_qty>0] * yearly_prices.loc[yearend,off_qty>0] 
+    gains = off_qty.clip(lower=0) * yearly_prices.loc[yearend]
+    
+    # 4. find losses = calculate money amounts of assets lower than allocation plans, based on off quantities
+    #   same as gains
+    #   The following is an example of the bad practice 
+    #   losses = off_qty.loc[off_qty<0] * yearly_prices.loc[yearend,off_qty<0]
+    losses = off_qty.clip(upper=0) * yearly_prices.loc[yearend]
+    
+    # 5. if the total gains are less than a minimum unit price of the loss assets,
+    #   skip rebalancing, because not a single minimum asset can be purchased
+    #
+    #   a. To find the minimum unit price of the loss assets,
+    #      find unit prices of loss assets
+    #
+    #   b. convert nonzero quantities of loss assets to 1: 
+    #      off_qty.clip(upper=0).where(off_qty.clip(upper=0)==0,1)
+    #   
+    #   c. multiply them by yearend prices, which results in loss unit prices
     loss_unit_prices = off_qty.clip(upper=0).where(off_qty.clip(upper=0)==0,1) * yearly_prices.loc[yearend]
+    
+    #   d. exclude zeros to accurately locate minimum unit price
+    #      loss_unit_prices.loc[loss_unit_prices!=0]
+    
+    #   e. sort loss unit prices that are not zero.
     loss_unit_prices = loss_unit_prices.loc[loss_unit_prices!=0].sort_values()
+    #   f. locate the minimum unit price of loss assets
     min_asset, min_unit_price = loss_unit_prices.index[0], loss_unit_prices[0]    
+    #   g. execute with the above conditions to skip the rebalancing
     if gains.sum() < min_unit_price:
         continue
     
@@ -136,15 +274,30 @@ for yearend in yearly_prices.index:
     # if gains.sum() < losses.min(): 
     #     continue
     
+
+    # 6. find asset quantities to restock
     # quantify asset quantities to restock from money amounts to restock
+    #   a. find rebalance ratio 
+    #       = find ratio of loss assets' off quantities
+    #       off quantities of only loss assets / sum of off quantities of only loss assets   
     rebalance_ratio = off_qty.clip(upper=0) / off_qty.clip(upper=0).sum()
+    #   b. find amounts ratio that can be distributed to buy restocking assets after selling gained assets
+    #       sum of gains * off quantities ratio of loss assets / yearend prices
+    #   c. to find quantities to restock,
+    #       round the restock quantitiy ratio above that can be distributed from selling gained assets
     rebalance_qty = (gains.sum()*rebalance_ratio/yearly_prices.loc[yearend]).round(decimals=1).astype('int')
+    #   d. find actual prices to restock
+    #       quantities to restock * yearend prices
+    #   e. sort the restock prices calculated
     rebalance_order = (rebalance_qty.abs()*yearly_prices.loc[yearend]).sort_values(ascending=False)
+    #   f. find the threshold to buy restocking assets
+    #       calculate cumulative sum of the above sorted restock prices
+    #       list restock prices of assets that are less than the total gains
     rebalance_assets = rebalance_order.cumsum() < gains.sum() 
 
-    # correct holdings accoringly
-    # rebalance_qty.loc[rebalance_assets] refers to assets to restock (plus values so add this from holdings)
-    # off_qty.loc[off_qty>0] refers to assets to sell (plus values so subract this from holdings)
+    # 7. correct holdings accoringly
+    #   rebalance_qty.loc[rebalance_assets] refers to assets to restock (plus values so add this from holdings)
+    #   off_qty.loc[off_qty>0] refers to assets to sell (plus values so subract this from holdings)
     holdings.loc[yearend] = holdings.loc[yearend] + rebalance_qty.loc[rebalance_assets] - off_qty.clip(lower=0)
 
 FX_included_prices = pd.DataFrame()
@@ -155,8 +308,8 @@ FX_included_prices
 
 portfolio_prices = {'FX_included': pd.DataFrame(), 'FX_not_included': pd.DataFrame()}
 for year in years:
-    portfolio_prices['FX_not_included'] = pd.concat([portfolio_prices['FX_not_included'], prices.loc[prices.index.year==year]*holdings.loc[yearend]])
-    portfolio_prices['FX_included'] = pd.concat([portfolio_prices['FX_included'], FX_included_prices.loc[FX_included_prices.index.year==year]*holdings.loc[yearend]])
+    portfolio_prices['FX_not_included'] = pd.concat([portfolio_prices['FX_not_included'], prices.loc[prices.index.year==year]*holdings.loc[holdings.index.year==year].iloc[0,:]])
+    portfolio_prices['FX_included'] = pd.concat([portfolio_prices['FX_included'], FX_included_prices.loc[FX_included_prices.index.year==year]*holdings.loc[holdings.index.year==year].iloc[0,:]])
 portfolio_daily_values = {'FX_not_included':portfolio_prices['FX_not_included'].sum(axis='columns'),
                           'FX_included':portfolio_prices['FX_included'].sum(axis='columns')}
 
@@ -171,12 +324,15 @@ ax1[0,0].plot(portfolio_daily_values['FX_not_included'])
 ax1[0,1].plot(portfolio_prices['FX_not_included'])
 ax1[1,0].plot(portfolio_daily_values['FX_included'])
 ax1[1,1].plot(portfolio_prices['FX_included'])
-plt.legend(portfolio_prices['FX_not_included'].columns)
+plt.legend(portfolio_prices['FX_not_included'].columns, loc='best')
 plt.show()
 
 port = holdings*yearly_prices
 allocation_diff = (port.divide(port.sum(axis='columns'), axis='index') - weights/weights.sum()) * 100
 allocation_diff.loc[allocation_diff.values>1][allocation_diff>1]
+
+
+
 
 
 daily_ret = prices.pct_change()
