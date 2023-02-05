03afb7559aa7e1611d8337fbefd982ed37d5f364
codebummer <holysmokes@hanmail.net>
2023-02-06 03:12:23 +0900

    Update indices_preprocessing.py

--- indices_preprocessing.py
+++ indices_preprocessing.py
@@ -11,6 +11,62 @@ from tqdm import tqdm
 from pprint import pprint
 import sqlite3
 
+def main():
+    # get data available from stooq.com
+    data = {}
+    start = datetime(1946, 1, 1)
+    # 'KORM2':'M2SYKR.M' M2 is not vailable
+    stooq = {'KOSPI':'^KOSPI', 'KOR10Y':'10KRY.B', 
+            'S&P500':'^SPX', 'NASDAQ':'^NDQ', 'US10Y':'10USY.B', 'US2Y':'2USY.B'}       
+
+    for key, value in tqdm(stooq.items()):
+        data[key] = stooq_data(value, start)
+
+    # get data available from yahoo finance
+    yahoo = {'JPY/KRW':'JPYKRW=X', 'USD/KRW':'KRW=X', 'EUR/KRW':'EURKRW=X', 'USD/CNY':'CNY=X'}
+    for key, value in tqdm(yahoo.items()):
+        data[key] = yahoo_data(value, start)
+
+    # import CRUDE, DXY, GOLD prices from json files
+    data['CRUDE'] = crude_data()
+    data['DXY'] = dxy_data()
+    data['GOLD'] = gold_data()
+
+    # sort values ascending by date
+    for key in data.keys():
+        data[key].sort_index(ascending=True, inplace=True)
+
+    # find earlest and latest dates in the dataset
+    ranges = date_range(data)
+    first = ranges.loc['start'].max()
+    last = ranges.loc['end'].min()
+
+    # make the dataset ranged in the same date period
+    iso_ranged = make_isorange(data, first, last)
+    show_info(iso_ranged)
+    show_len(iso_ranged)
+    date_range(iso_ranged)
+    show_columns(iso_ranged)
+
+    # pull only 'Close' values from the dictionary with multiple dataframes and make them one dataframe
+    iso_columned = pull_isocolumn(iso_ranged, 'Close')
+    pprint(iso_columned)
+    show_len(iso_columned)
+    date_range(iso_columned)
+    show_columns(iso_columned)
+
+    # keep data only with same dates as KOSPI value dates
+    isodated = make_isodate(iso_columned, iso_columned['KOSPI'].index)
+    isodated.info()
+    isodated.describe()
+    isodated.corr()
+
+    sns.heatmap(isodated)
+    plt.savefig('./indices_heatmap.png')
+
+    sns.pairplot(isodated)
+    plt.savefig('./indices_pairplot.png')
+
 
 def show_info(dic):
     for key, value in dic.items():
@@ -120,57 +176,5 @@ def stooq_data(ticker, start):
 def yahoo_data(ticker, start):
     return pdr.get_data_yahoo(ticker, start, datetime.today())
 
-# get data available from stooq.com
-data = {}
-start = datetime(1946, 1, 1)
-# 'KORM2':'M2SYKR.M' M2 is not vailable
-stooq = {'KOSPI':'^KOSPI', 'KOR10Y':'10KRY.B', 
-         'S&P500':'^SPX', 'NASDAQ':'^NDQ', 'US10Y':'10USY.B', 'US2Y':'2USY.B'}       
-
-for key, value in tqdm(stooq.items()):
-    data[key] = stooq_data(value, start)
-
-# get data available from yahoo finance
-yahoo = {'JPY/KRW':'JPYKRW=X', 'USD/KRW':'KRW=X', 'EUR/KRW':'EURKRW=X', 'USD/CNY':'CNY=X'}
-for key, value in tqdm(yahoo.items()):
-    data[key] = yahoo_data(value, start)
-
-# import CRUDE, DXY, GOLD prices from json files
-data['CRUDE'] = crude_data()
-data['DXY'] = dxy_data()
-data['GOLD'] = gold_data()
-
-# sort values ascending by date
-for key in data.keys():
-    data[key].sort_index(ascending=True, inplace=True)
-
-# find earlest and latest dates in the dataset
-ranges = date_range(data)
-first = ranges.loc['start'].max()
-last = ranges.loc['end'].min()
-
-# make the dataset ranged in the same date period
-iso_ranged = make_isorange(data, first, last)
-show_info(iso_ranged)
-show_len(iso_ranged)
-date_range(iso_ranged)
-show_columns(iso_ranged)
-
-# pull only 'Close' values from the dictionary with multiple dataframes and make them one dataframe
-iso_columned = pull_isocolumn(iso_ranged, 'Close')
-pprint(iso_columned)
-show_len(iso_columned)
-date_range(iso_columned)
-show_columns(iso_columned)
-
-# keep data only with same dates as KOSPI value dates
-isodated = make_isodate(iso_columned, iso_columned['KOSPI'].index)
-isodated.info()
-isodated.describe()
-isodated.corr()
-
-sns.heatmap(isodated)
-plt.savefig('./indices_heatmap.png')
-
-sns.pairplot(isodated)
-plt.savefig('./indices_pairplot.png')
+if __name__ == '__main__':
+    main()
