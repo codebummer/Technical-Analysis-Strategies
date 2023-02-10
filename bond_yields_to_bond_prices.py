aa9448e254f9cf220f53346fc8dbb4a91e636848
codebummer <holysmokes@hanmail.net>
2023-02-11 03:15:23 +0900

    Update bond_yields_to_bond_prices.py

--- bond_yields_to_bond_prices.py
+++ bond_yields_to_bond_prices.py
@@ -21,13 +21,13 @@ def calc_bond_price(assgined_amount, yields):
     bond_prices[0] = fixed_income
     return bond_prices
 
-# The following(groupby() operation) is a lot slowwer than the below (loc operation)
+# The following operations are expensive.
 bond_prices = pd.Series()
 for year in tqdm(assets.index.year):
     add = calc_bond_price(invested*weights['US10Y'], assets['US10Y'].groupby(assets.index.year).get_group(year))
     bond_prices = pd.concat([bond_prices, add])
 
-# Use the following, because it's faster
+# alternative way to implement the above. Also expensive
 bond_prices = pd.Series()
 for year in tqdm(assets.index.year):
     add = calc_bond_price(invested*weights['US10Y'], assets['US10Y'].loc[assets.index.year==year])
