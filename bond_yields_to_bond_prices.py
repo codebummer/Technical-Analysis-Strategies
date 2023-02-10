5b95275b1f8a072702e1edc0ea56094f8ab876a7
codebummer <holysmokes@hanmail.net>
2023-02-11 02:23:40 +0900

    Create bond_yields_to_bond_prices.py

---
+++ bond_yields_to_bond_prices.py
@@ -0,0 +1,18 @@
+# convert bond yields to bonds prices
+def calc_bond_price(assgined_amount, yields):
+    '''
+    aggsigned_amount: initial cash amount assigned to bonds in the working investment strategy,
+    yields: a pandas series or dataframe that holds daily bond yields
+    '''
+    # calculate the inital fixed income amount from the initial cash amount assigned to invest in bonds
+    # 
+    # bonds yields amount (or fixed income amount) at the time of purchase: fixed_income (in the below code block)
+    # = bond yield at the time of purchase (in decimal digits, not in percent) + 1(to include the principal) * the initial cash amount to invest in bonds
+    fixed_income = assgined_amount * (yields.values[0]/100+1)
+
+    # the later market prices of the above fixed income amount at the time of purchase
+    # = [bond yield change (in decimal digits, not in percent)
+    # * -1(to apply the negative relationship between bond prices and bond yields) 
+    # + 1(to include the principal) ] * the initial fixed income amount at the time of purchase
+    # Note: pandas.DataFrame.pct_change() does not generate changes in percent, but in decimal digits.
+    return yields.pct_change().multiply(-1).add(1) * fixed_income 
