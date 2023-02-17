ab61b3e3c674dab03dcc9932568e5ed7b8dc0fb7
codebummer <holysmokes@hanmail.net>
2023-02-18 06:45:26 +0900

    Update DART_EDGAR.py

--- DART_EDGAR.py
+++ DART_EDGAR.py
@@ -9,34 +9,37 @@ import OpenDartReader
 import zipfile, io, sys, json
 import sqlite3
 import time
+from tqdm import tqdm
 # import requests, json, xmltodict
 from  urllib.request import urlopen
 from selenium import webdriver
 sys.path.append('\myprojects\MarketDB')
-from cert_key import *
+from DART_key import *
 sns.set_theme(style='ticks')
 
 # DART link
 url = 'https://opendart.fss.or.kr/api/corpCode.xml?crtfc_key='
-param = CERT_KEY
+param = DART_KEY
 
 os.chdir(r'D:\myprojects')
-# Run when you don't have the entire company list
-# This list also contains unlisted companies 
-with urlopen(url+param) as response:
-    with zipfile.ZipFile(io.BytesIO(response.read())) as zip:
-        zip.extractall('./MarketDB/corpCode')
-        print('All Business List Successfully Received')
 
 # Read the existing company list
-corps = pd.read_xml('./MarketDB/corpCode/CORPCODE.xml')
-corps.dropna(inplace=True)
-corps = corps.reset_index().drop('index', axis='columns')
-# remove = corps.index[corps['corp_name']=='루트원플러스']
-# corps = corps.drop(remove)
+def get_company_list():
+    # Run when you don't have the entire company list
+    # This list also contains unlisted companies 
+    with urlopen(url+param) as response:
+        with zipfile.ZipFile(io.BytesIO(response.read())) as zip:
+            zip.extractall('./MarketDB/corpCode')
+            print('All Business List Successfully Received')
+    corps = pd.read_xml('./MarketDB/corpCode/CORPCODE.xml')
+    corps.dropna(inplace=True)
+    corps = corps.reset_index().drop('index', axis='columns')
+    return corps
+    # remove = corps.index[corps['corp_name']=='루트원플러스']
+    # corps = corps.drop(remove)
 
 # Read the prior database downloaded 
-with sqlite3.connect('./MarketDB/fins_2022_Q1.db') as db:
+with sqlite3.connect('./MarketDB/KoreaFins/fins_2022_Q3.db') as db:
     query = '''SELECT * FROM sqlite_master WHERE type='table';'''
     tables = db.cursor().execute(query).fetchall()
 
@@ -44,27 +47,30 @@ with sqlite3.connect('./MarketDB/fins_2022_Q1.db') as db:
 listed_corps = [table[1].split('_')[0] for table in tables]
 
 # instantiate the Open DART class
-dart = OpenDartReader(CERT_KEY)
+dart = OpenDartReader(DART_KEY)
+
+years = [2021, 2020, 2019, 2018]
 
 # reports = {'1분기보고서':'11013', '반기보고서':'11012', '3분기보고서':'11014', '사업보고서':'11011'}
-reports = {'1분기보고서':'11013'}
-for corp in listed_corps:
-# for corp in corps['corp_name']:
-# for corp in corps['corp_name'].values[remove[0]:]:
-    # for year in range(2022, 2014, -1):
-    year = 2022
-    for title, report in reports.items():
-        try:
-            add = dart.finstate_all(corp, year, report)
-            # without time.sleep(0.5), an error will occur
-            time.sleep(0.5)
-        except:
-            time.sleep(0.5)
-            continue
-        if len(add) != 0:
-            with sqlite3.connect('./MarketDB/fins_2022_Q1.db') as db:
-                add.to_sql(corp+'_'+title, db, if_exists='replace', index=False)
-            # dart.finstate_all(code, 2021, report, 'OFS')
+reports = {'사업보고서':'11011', '3분기보고서':'11014', '반기보고서':'11012', '1분기보고서':'11013'}
+
+for year in tqdm(years):
+    for title, report in tqdm(reports.items()):
+    # for corp in corps['corp_name']:
+    # for corp in corps['corp_name'].values[remove[0]:]:
+        # for year in range(2022, 2014, -1):
+        for corp in tqdm(listed_corps):
+            try:
+                add = dart.finstate_all(corp, year, report)
+                # without time.sleep(0.5), an error will occur
+                time.sleep(0.5)
+            except:
+                time.sleep(0.5)
+                continue
+            if len(add) != 0:
+                with sqlite3.connect(f'./MarketDB/fins_{year}_{title}.db') as db:
+                    add.to_sql(corp+'_'+title, db, if_exists='replace', index=False)
+                # dart.finstate_all(code, 2021, report, 'OFS')
 
 
 stocks_info = {}
